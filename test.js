c1993c10b3d8c2c9d669c6a3743fe91258a400fc
jack eichner <jack8191@gmail.com>
2018-05-10 10:04:10 +0000

    automatically backed up by learn

--- test.js
+++ test.js
@@ -3,5 +3,5 @@ console.log(word)
 word = 'dog'
 console.log(word)
 
-var a = 5, b = 2, c = 3, d = 'hello', e = 'goodbye';
+var a = 5, b = 2, c = 3, d = 'hello', e = 'goodbye'
 console.log(a,b,c,d,e)
